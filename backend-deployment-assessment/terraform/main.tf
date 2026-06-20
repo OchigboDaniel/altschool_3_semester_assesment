8c765e158d184dd9dbdf16dec0019f2cf2d5417b
OchigboDaniel <113383346+OchigboDaniel@users.noreply.github.com>
2026-06-20 15:09:18 +0000

    feat:add subnet resource

---
+++ backend-deployment-assessment/terraform/main.tf
@@ -0,0 +1,59 @@
+# DATA SOURCE - Get the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+#VPC
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+#Subnet
+# Public Subnets
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  availability_zone = "${var.aws_region}${count.index == 0 ? "a" : "b"}"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-subnet-${count.index + 1}"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = "${var.aws_region}${count.index == 0 ? "a" : "b"}"
+
+  tags = {
+    Name = "${var.project_name}-private-subnet-${count.index + 1}"
+  }
+}
+
+#Internet Gateway
+
+#Route Table
+
+#Route relationship
--- backend-deployment-assessment/terraform/provider.tf
+++ backend-deployment-assessment/terraform/provider.tf
@@ -2,11 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "6.51.0"
     }
   }
 }
 
 provider "aws" {
+  # Configuration options
   region = var.aws_region
 }
